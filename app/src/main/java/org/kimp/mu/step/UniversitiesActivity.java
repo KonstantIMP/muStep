933eba2e13a00fdb27fca28be4a7636f19b0264e
KonstantIMP <mihedovkos@gmail.com>
2022-01-22 19:35:20 +0300

    Add back button for the universities activity

--- app/src/main/java/org/kimp/mu/step/UniversitiesActivity.java
+++ app/src/main/java/org/kimp/mu/step/UniversitiesActivity.java
@@ -1,14 +1,29 @@
 package org.kimp.mu.step;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+
+import org.kimp.mu.step.databinding.ActivityUniversitiesBinding;
 
 public class UniversitiesActivity extends AppCompatActivity {
+
+    private ActivityUniversitiesBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_universities);
+
+        binding = ActivityUniversitiesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.uaBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
